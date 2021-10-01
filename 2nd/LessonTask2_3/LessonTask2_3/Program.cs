a40692e26163fa3ae15ae39d825e0dc8f74cd90e
yukonopleva <sunshine331@rambler.ru>
2021-10-01 12:06:35 +0300

    2nd homework is ready

---
+++ 2nd/LessonTask2_3/LessonTask2_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonTask2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currData = new DateTime(2021, 9, 30, 10, 28, 42);
+            int positNum = 1;
+            decimal positCost = 980.00M;
+            decimal persMoney = 1000.00M;
+            int kassirNum = 1;
+            int checkNum = 25;
+
+            //Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("     НОВЫЙ ЧЕК     ");
+            Console.WriteLine("     АМС-100к      ");
+            Console.WriteLine(" ИНН. 007700000000 ");
+            Console.WriteLine("    НМ 20547316    ");
+            Console.WriteLine("      КЛ 0196      ");
+            Console.WriteLine($"{currData.ToString("d")}    {currData.ToString("HH:mm")}");
+            Console.WriteLine("-------------------");
+            Console.WriteLine($" {positNum}     {positCost}");
+            Console.WriteLine("-------------------");
+            Console.WriteLine($"ИТОГ*********{positCost}");
+            Console.WriteLine($"НАЛИЧ       {persMoney}");
+            Console.WriteLine($"СДАЧА         {persMoney - positCost}");
+            Console.WriteLine("     СПАСИБО!     ");
+            Console.WriteLine($"КАССИР {kassirNum}  ЧЕК  {checkNum.ToString("0000")}");
+            Console.WriteLine("*ПФП  000000005050*");
+            Console.WriteLine("    ЭКЛЗ 2699573369");
+            Console.WriteLine("   00009972 #044887");
+        }
+    }
+}
---
+++ 2nd/LessonTask2_4/LessonTask2_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LessonTask2_4
+{
+	class Program
+	{
+		[Flags]
+		public enum WeekDays
+		{
+			None = 0b_0000000,
+			Monday = 0b_0000001,
+			Tuesday = 0b_0000010,
+			Wednesday = 0b_0000100,
+			Thursday = 0b_0001000,
+			Friday = 0b_0010000,
+			Saturday = 0b_0100000,
+			Sunday = 0b_1000000,
+		}
+
+		static void Main(string[] args)
+		{
+			WeekDays workDays = (WeekDays)0b_0111110;
+
+			Console.WriteLine($"Working days: {workDays}");
+
+		}
+	}
+}
