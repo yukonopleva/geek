1ddc826db7c3540dc85e2e27e1695b935f06e9e8
yukonopleva <sunshine331@rambler.ru>
2021-10-08 14:12:52 +0300

    4rd homeworks is ready

---
+++ 4rd/LessonTask4_1/LessonTask4_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonTask3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName1 = "Ivan";
+            string lastName1 = "Ivanov";
+            string patronymic1 = "Ivanovich";
+
+            Console.WriteLine(GetFullName(firstName1, lastName1, patronymic1));
+
+            string firstName2 = "Petr";
+            string lastName2 = "Petrov";
+            string patronymic2 = "Petrovich";
+
+            Console.WriteLine(GetFullName(firstName2, lastName2, patronymic2));
+
+            string firstName3 = "Aleksandr";
+            string lastName3 = "Aleksandrov";
+            string patronymic3 = "Aleksandrovich";
+
+            Console.WriteLine(GetFullName(firstName3, lastName3, patronymic3));
+        }
+
+            public static string GetFullName(string firstName, string lastName, string patronymic)
+        { 
+            string fullName = firstName + " " + lastName + " " + patronymic;
+            return fullName;
+        }
+    }
+}
---
+++ 4rd/LessonTask4_2/LessonTask4_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonTask4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input numbers separated by space:");
+            string numbers = Console.ReadLine();
+
+            Console.WriteLine($"Sum of numbers: {GetNumSum(numbers)}");
+        }
+
+        static int GetNumSum(string numbers)
+        {
+            string[] nums = numbers.Split(' ');
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+                sum = sum + Convert.ToInt32(nums[i]);
+
+            return sum;
+        }
+    }
+}
